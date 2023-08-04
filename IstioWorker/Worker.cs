ae8ffe76b99299833f69743f814a5ad7dffa9231
edercarloscosta <edercarlosdacosta@hotmail.com>
2023-08-04 19:37:07 +0100

    improving the project

--- IstioWorker/Worker.cs
+++ IstioWorker/Worker.cs
@@ -1,4 +1,5 @@
 using Grpc.Net.Client;
+using IstioGrpc;
 
 namespace IstioWorker;
 
@@ -6,28 +7,23 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+    public Worker(ILogger<Worker> logger) => _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         using var channel = GrpcChannel.ForAddress("http://grpc-server-service:80");
-        var client = new Greeter.GreeterClient(channel);
-
-        var counter = 1;
+        var client = new Joker.JokerClient(channel);
+        
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        
+            var reply = await client.NewJokeAsync(new JokeRequest());
 
-            var reply = await client.SayHelloAsync(new HelloRequest
-            {
-                Name = $"Worker {counter}"
-            });
-            counter++;
-            
-            _logger.LogInformation($"Response: {reply.Message}");
+            Console.WriteLine($"Setup: {reply.Setup}");
+            await Task.Delay(3000, stoppingToken);
+            Console.WriteLine($"Punchline: {reply.Punchline}\n");
+
             await Task.Delay(20000, stoppingToken);
         }
     }
