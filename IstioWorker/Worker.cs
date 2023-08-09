cc2749e7c3e21dc80e3d0251484598f2fc6f0d20
edercarloscosta <edercarlosdacosta@hotmail.com>
2023-08-09 14:59:07 +0100

    adding NodePort type on Service and add stopwatch

--- IstioWorker/Worker.cs
+++ IstioWorker/Worker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Grpc.Net.Client;
 using IstioGrpc;
 using Polly;
@@ -16,7 +17,9 @@ public class Worker : BackgroundService
             .Or<Grpc.Core.RpcException>()
             .WaitAndRetry(5, retryAttempt 
                 => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
-            
+        
+        var stopwatch = new Stopwatch();
+        
         using var channel = GrpcChannel.ForAddress("http://istiogrpc:80");
         var client = retryPolicy.Execute(() => new Joker.JokerClient(channel));
         
@@ -24,12 +27,24 @@ public class Worker : BackgroundService
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
         
+            // Start the stopwatch before making the gRPC call
+            stopwatch.Start();
+            
             var reply = await client.NewJokeAsync(new JokeRequest());
-
+            
+            // Stop the stopwatch after the gRPC call
+            stopwatch.Stop();
+            
             Console.WriteLine($"Setup: {reply.Setup}");
             await Task.Delay(3000, stoppingToken);
-            Console.WriteLine($"Punchline: {reply.Punchline}\n");
+            Console.WriteLine($"Punchline: {reply.Punchline}");
+            
+            // Print the elapsed time for the gRPC call
+            Console.WriteLine($"Time taken for gRPC call: {stopwatch.Elapsed}\n");
 
+            // Reset the stopwatch for the next iteration
+            stopwatch.Reset();
+            
             await Task.Delay(20000, stoppingToken);
         }
     }
