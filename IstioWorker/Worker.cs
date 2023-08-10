68bf6354d8478aee85f0912662fc0c8bbac08d19
edercarloscosta <edercarlosdacosta@hotmail.com>
2023-08-10 12:19:27 +0100

    removing unnecessary usings

--- IstioWorker/Worker.cs
+++ IstioWorker/Worker.cs
@@ -1,12 +1,6 @@
-using System;
 using System.Diagnostics;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 using Grpc.Net.Client;
 using IstioGrpc;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Polly;
 
 namespace IstioWorker;
